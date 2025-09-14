880e1b94399ddf99d38fac896c989c71971a3ff7
Georffrey <georffreyarevalo@gmail.com>
2025-09-13 19:23:56 -0500

    Add Exceptions handlers in config security

--- infrastructure/driven-adapters/security/src/main/java/co/com/crediya/security/config/SecurityConfig.java
+++ infrastructure/driven-adapters/security/src/main/java/co/com/crediya/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package co.com.crediya.security.config;
 
+import co.com.crediya.security.exceptions.handlers.AccessDeniedExceptionHandler;
+import co.com.crediya.security.exceptions.handlers.UnauthorizedExceptionHandler;
 import co.com.crediya.security.util.KeysUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -13,9 +15,12 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     private final KeysUtil keysUtil;
+    private final AccessDeniedExceptionHandler accessDeniedExceptionHandler;
+    private final UnauthorizedExceptionHandler unauthorizedExceptionHandler;
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
 
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
@@ -28,6 +33,11 @@ public class SecurityConfig {
                 .logout(ServerHttpSecurity.LogoutSpec::disable)
                 .oauth2ResourceServer( oauth2 ->
                         oauth2.jwt( jwt -> jwt.publicKey( keysUtil.loadPublicKey() ) )
+                )
+                .exceptionHandling( exceptionHandlingSpec ->
+                        exceptionHandlingSpec
+                                .authenticationEntryPoint(unauthorizedExceptionHandler)
+                                .accessDeniedHandler(accessDeniedExceptionHandler)
                 ).build();
 
     }
---
+++ infrastructure/driven-adapters/security/src/main/java/co/com/crediya/security/exceptions/handlers/AccessDeniedExceptionHandler.java
@@ -0,0 +1,18 @@
+package co.com.crediya.security.exceptions.handlers;
+
+
+import co.com.crediya.exceptions.CrediyaForbiddenException;
+import co.com.crediya.exceptions.enums.ExceptionMessages;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.server.authorization.ServerAccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AccessDeniedExceptionHandler implements ServerAccessDeniedHandler {
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange, AccessDeniedException denied) {
+        return Mono.error( new CrediyaForbiddenException(ExceptionMessages.DO_NOT_ACCESS_RESOURCE.getMessage()));
+    }
+}
